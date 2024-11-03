cd076e7efae315ff23c49dc1674f72fb42c1248f
Eren Kulaksız <erenkulaksz@gmail.com>
2024-11-04 02:35:07 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <main className="h-screen w-full bg-red-400 flex flex-row">
+      selam naber
+      <Button>selam</Button>
+    </main>
+  );
+}
+
+export default App;
